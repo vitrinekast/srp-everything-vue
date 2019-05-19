b8b8f5f317f6f6585926e1c9e5647adf744fe121
Rosa Schuurmans <rosa@wetteloos.nl>
2019-05-19 14:57:48 +0200

    started scss framework

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <router-link to="/">Overview</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/submit">Submit your stuff</router-link>
+    <nav>
+      <router-link to="/">Overview</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/submit">Submit your stuff</router-link>
+    </nav>
     <router-view/>
   </div>
 </template>
